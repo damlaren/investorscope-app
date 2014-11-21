f75285fdf5673fb365d0dd66be0a875f01197cd7
David McLaren <damlaren@gmail.com>
2014-11-20 18:54:52 -0800

    ported app over from old location

--- app.py
+++ app.py
@@ -3,6 +3,15 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route('/home.html')
 @app.route('/')
-def hello():
-    return render_template('test.html')
+def home():
+    return render_template('home.html')
+
+@app.route('/question.html')
+def question():
+    return render_template('question.html')
+
+@app.route('/stock.html')
+def stock():
+    return render_template('stock.html')
