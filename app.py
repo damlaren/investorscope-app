eeff9371d7ee669ef7d57341a02554027d47a937
David McLaren <damlaren@gmail.com>
2014-11-22 13:45:35 -0800

    first functioning use of Stock model

--- app.py
+++ app.py
@@ -15,42 +15,16 @@ app.config['MONGODB_SETTINGS'] = {
 }
 db = MongoEngine(app)
 
-import models # must be after db created
-
 # ------------------------------
 # TEST: Ensure functional mongo
 # ------------------------------
 
-# class Person(db.Document):
-# 	name = db.StringField()
-# 	meta = {
-# 		'collection': 'users'
-# 	}
-
-# collection = Person._get_collection()
-# collection.insert({'name': 'david'})
+from models import Stock
+collection = Stock._get_collection()
+collection.insert({'ticker': 'AAPL', 'name': 'Apple Inc.', 'latest_price': 120})
 
 # ------------------------------
 # END TEST: Ensure func. mongo
 # ------------------------------
 
-@app.route("/home.html")
-@app.route("/")
-def home():
-    return render_template("home.html")
-
-# search form submission from main page.
-# this would be better as an auto-complete function.
-@app.route("/submit_search", methods=["POST"])
-def submit_search():
-    ticker = request.form["search_string"]
-    return redirect(url_for("stock", ticker=ticker))
-
-@app.route("/question.html")
-def question():
-    return render_template("question.html")
-
-@app.route("/stock.html")
-def stock():
-    ticker = request.args.get("ticker","AAPL").upper()
-    return render_template("stock.html", ticker=ticker)
+import views # All the routing is in here.
--- models.py
+++ models.py
@@ -18,12 +18,13 @@ class Stock(db.DynamicDocument):
 
     # Get a stock from DB by ticker.
     # - ticker_string: Stock ticker symbol. Doesn't have to be uppercase.
-    def get_stock_from_db(self, ticker_string):
+    @staticmethod
+    def get_stock_from_db(ticker_string):
         if not ticker_string:
             raise ValueError()
 
-        sub_collection = Stock.objects(ticker=ticker_string.upcase())
+        sub_collection = Stock.objects(ticker=ticker_string.upper())
         results_size = len(sub_collection)
-        if results_size() == 0:
+        if results_size == 0:
             return Stock()
         return sub_collection[0]
